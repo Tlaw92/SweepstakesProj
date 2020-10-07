e41a5d7525d7b211671442b84785ce66051bdbd0
Travis Lawson <travislawclimb@gmail.com>
2020-10-07 11:38:16 -0700

    Set up skeleton

--- SweepstakesProject/Simulation.cs
+++ SweepstakesProject/Simulation.cs
@@ -9,6 +9,29 @@ namespace SweepstakesProject
     class Simulation
     {
 
+        //variables
+        public Contestant contestant1;
+        public Contestant contestant2;
+        public Sweepstakes sweepstakes1;
+
+        //Constructor
+        public Simulation()
+        {
+
+        }
+
+
+        //Methods
+        public void RunMain()
+        {
+            sweepstakes1 = new Sweepstakes("October Sweep");
+            contestant1 = new Contestant("Jim", "Baron", "JimBaron@gmail.com", sweepstakes1.GetRandomNumber(50));
+            contestant2 = new Contestant("Tim", "Burton", "TimBurton@gmail.com", sweepstakes1.GetRandomNumber(50));
+
+            sweepstakes1.RegisterContestant(contestant1);
+            sweepstakes1.RegisterContestant(contestant2);
+            Console.ReadLine();
+        }
 
     }
 }
