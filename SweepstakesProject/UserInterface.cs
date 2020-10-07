cf8b26c23ab6e91a36e597e1b6e34abdf82363d8
Travis Lawson <travislawclimb@gmail.com>
2020-10-07 11:42:33 -0700

    Set up skeleton.

--- SweepstakesProject/UserInterface.cs
+++ SweepstakesProject/UserInterface.cs
@@ -10,7 +10,22 @@ namespace SweepstakesProject
     {
         //This will be for all the user input and output. (Console.Write/Read Lines)
 
-            //One example would be the functionality to assign a Contestant object a
-            //fist name, last name, email address, and registration number.
+        //One example would be the functionality to assign a Contestant object a
+        //fist name, last name, email address, and registration number.
+
+        //variables
+
+
+        //constructor
+
+
+        //methods 
+
+        static string GetUserInput(string output)
+        {
+            Console.WriteLine(output);
+            return Console.ReadLine();
+        }
+
     }
 }
