ed92bb81be396a725f4b8c7a39455c4cbe36d8b8
Travis Lawson <travislawclimb@gmail.com>
2020-10-07 11:32:07 -0700

    Set up skeleton

--- SweepstakesProject/ISweepstakesManager.cs
+++ SweepstakesProject/ISweepstakesManager.cs
@@ -8,5 +8,13 @@ namespace SweepstakesProject
 {
     interface ISweepstakesManager
     {
+        //void InsertSweepstakes(Sweepstakes sweepstakes)
+        //{
+
+        //}
+        //Sweepstakes GetSweepstakes()
+        //{
+
+        //}
     }
 }
